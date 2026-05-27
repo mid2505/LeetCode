d555a9430ac368606de883b91f8c905a51144a48
Pothiraju P V <pothiraju010_bcs27@mepcoeng.ac.in>
2026-05-27 15:04:01 +0530

    Time: 22 ms (50%), Space: 48.3 MB (34.3%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int n = word.length();
+        int[] arr = new int[26];
+        int[] firstOccurence = new int[26];
+        Arrays.fill(firstOccurence,-1);
+        for(int i=0;i<n;i++){
+            char ch = word.charAt(i);
+            int t = (int)ch;
+            if(t >= 65 && t <= 90 && arr[ch-'A']==0 && firstOccurence[ch-'A']==-1){
+                arr[ch-'A']++;
+                firstOccurence[ch-'A']=i;
+            }
+        }
+        // display(arr);
+        // display(firstOccurence);
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            char ch = word.charAt(i);
+            int t = (int)ch;
+            if(t >= 97 && t <= 122 && firstOccurence[ch-'a']<i) arr[ch-'a']=-1;
+        }
+        for(int i=0;i<n;i++){
+            char ch = word.charAt(i);
+            int t = (int)ch;
+            if(t >= 97 && t <= 122 && arr[ch-'a']==1 && firstOccurence[ch-'a']>i){
+                arr[ch-'a']++;
+                ans++;
+            }
+        }
+        return ans;
+    }
+    public static void display(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        return;
+    }
+}
