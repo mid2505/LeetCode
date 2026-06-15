3b9e3272f25185ae011417287006b57709827c81
Pothiraju P V <pothiraju010_bcs27@mepcoeng.ac.in>
2026-06-15 23:05:23 +0530

    Time: 4 ms (38.84%), Space: 44.3 MB (5.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if("({[".indexOf(ch+"")!=-1) st.push(ch);
+            else{
+                if(st.isEmpty()) return false;
+                char top = st.pop();
+                if(ch=='}' && top!='{') return false;
+                if(ch==']' && top!='[') return false;
+                if(ch==')' && top!='(') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
