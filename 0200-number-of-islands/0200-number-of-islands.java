667a5e6addb55e24da39e437c89a531c698338ca
Pothiraju P V <pothiraju010_bcs27@mepcoeng.ac.in>
2026-06-18 01:12:24 +0530

    Time: 13 ms (5.11%), Space: 51.6 MB (83.8%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] vis = new int[m][n];
+        int[] row = {-1,0,1,0};
+        int[] col = {0,1,0,-1};
+        int count = 0;
+        Deque<List<Integer>> q = new ArrayDeque<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis[i][j]==0 && grid[i][j]=='1'){
+                    count++;
+                    vis[i][j] = 1;
+                    q.offerLast(List.of(i,j));
+                    while(!q.isEmpty()){
+                        List<Integer> l = q.pollFirst();
+                        for(int x=0;x<4;x++){
+                            int r = l.get(0)+row[x];
+                            int c = l.get(1)+col[x];
+                            if(r>=0 && r<m && c>=0 && c<n && grid[r][c]=='1' && vis[r][c]==0){
+                                vis[r][c] = 1;
+                                q.offerLast(List.of(r,c));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
