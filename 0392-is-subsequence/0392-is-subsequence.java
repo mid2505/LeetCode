ffb818e0d7ab712c025e3a4748adf32ef31ec88b
Pothiraju P V <pothiraju010_bcs27@mepcoeng.ac.in>
2026-06-18 11:33:44 +0530

    Time: 2 ms (70.71%), Space: 42.7 MB (62.91%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()>t.length()) return false;
+        if(s.length()==0) return true;
+        int i = 0;
+        int j = 0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j++)){
+                i++;
+            }
+            if(i==s.length()) return true;
+        }
+        return false;
+    }
+}
