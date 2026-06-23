49201aa8d10d5a5346afe89830e7eb8deb601fa9
Pothiraju P V <pothiraju010_bcs27@mepcoeng.ac.in>
2026-06-24 00:16:42 +0530

    Time: 2 ms (19.88%), Space: 43.3 MB (13.54%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String replaceDigits(String s) {
+        StringBuilder ans = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(i%2==0){
+                ans.append(s.charAt(i));
+            }
+            else{
+                ans.append(""+(char)(((int)s.charAt(i-1))+Integer.parseInt(""+s.charAt(i))));
+            }
+        }
+        return ans.toString();
+    }
+}
